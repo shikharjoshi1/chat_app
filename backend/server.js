e80a28fca975f09873bbaa0130600e64a634363e
Shikhar Joshi <Sharma.277@wright.edu>
2024-04-03 04:19:30 -0400

    Socket.io setup and implementation

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,9 @@ const { chats } = require("./data/data");
 const cors = require("cors");
 const connectDB = require("./config/database");
 // const userRoutes = express.Router();
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-
-
 
 const app = express();
 dotenv.config();
@@ -30,9 +28,45 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+//Web socket initialization and function call
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection",(socket)=>{
+  console.log('connected to socket.io');
+
+  socket.on('setup',(userData)=>{
+    socket.join(userData._id);
+    // console.log(userData._id);
+    socket.emit('connected');
+  });
+
+  socket.on('join chat', (room)=>{
+    socket.join(room);
+    console.log("User Joined Room:"+ room);
+  })
+
+  socket.on('new message',(newMessageReceived)=>{
+    var chat = newMessageReceived.chat;
+
+    if(!chat.users) return console.log('chat.users not defined');
+
+    chat.users.forEach(user=>{
+      if(user._id == newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit('message received', newMessageReceived);
+
+    });
+  })
+
+})
--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -10,39 +10,49 @@ import {
   Input,
   useToast,
 } from "@chakra-ui/react";
-import './style.css';
+import "./style.css";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { getSender, getSenderDetails } from "../config/ChatLogic";
 import ProfileModal from "./miscellaneous/ProfileModal";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import axios from "axios";
 import ScrollableChat from "./ScrollableChat";
+import io from "socket.io-client";
+
+const ENDPOINT = "http://localhost:5000";
+var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [newMessage, setNewMessage] = useState();
+  const [socketConnected, setSocketConnected] = useState(false); //use state for socket.io
+
   const toast = useToast();
 
   const { user, selectedChat, setSelectedChat } = ChatState();
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  const fetchMessages = async ()=>{
-    if(!selectedChat) return;
+  const fetchMessages = async () => {
+    if (!selectedChat) return;
 
     try {
-      const config ={
-        headers:{
+      const config = {
+        headers: {
           Authorization: `Bearer ${user.token}`,
-        }
+        },
       };
       setLoading(true);
-      const {data} = await axios.get(`http://localhost:5000/api/message/${selectedChat._id}`,config);
+      const { data } = await axios.get(
+        `http://localhost:5000/api/message/${selectedChat._id}`,
+        config
+      );
 
       console.log(messages);
       setMessages(data);
       setLoading(false);
 
+      socket.emit("join chat", selectedChat._id);
     } catch (error) {
       toast({
         title: "Error Occured!",
@@ -53,32 +63,57 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
         position: "bottom",
       });
     }
-  }
+  };
+
+  useEffect(() => {
+    socket = io(ENDPOINT);
+    socket.emit("setup", user);
+    socket.on("connection", () => setSocketConnected(true));
+  }, []);
+
 
   useEffect(() => {
     fetchMessages();
+
+    selectedChatCompare = selectedChat;
   }, [selectedChat]);
-  
+
+  useEffect(() => {
+    socket.on("message received", (newMessageReceived) => {
+      if (
+        !selectedChatCompare ||
+        selectedChatCompare._id !== newMessageReceived.chat._id
+      ) {
+        //give notification
+      } else {
+        setMessages([...messages, newMessageReceived]);
+      }
+    });
+  });
 
   const sendMessage = async (event) => {
-    if(event.key=== "Enter" && newMessage){
+    if (event.key === "Enter" && newMessage) {
       try {
-        const config ={
-          headers:{
-            "Content-Type":"application/json",
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
             Authorization: `Bearer ${user.token}`,
-          }
+          },
         };
         setNewMessage("");
-        const {data} = await axios.post('http://localhost:5000/api/message',{
-          content: newMessage,
-          chatId: selectedChat._id,
-        },config);
+        const { data } = await axios.post(
+          "http://localhost:5000/api/message",
+          {
+            content: newMessage,
+            chatId: selectedChat._id,
+          },
+          config
+        );
 
         console.log(data);
+
+        socket.emit('new message', data);
         setMessages([...messages, data]);
-
-
       } catch (error) {
         toast({
           title: "Error Occured!",
@@ -91,6 +126,8 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       }
     }
   };
+
+ 
   const typingHandler = (e) => {
     setNewMessage(e.target.value);
 
@@ -123,34 +160,32 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             justifyContent="space-between"
             alignItems="center"
           > */}
-            {isSmallScreen && ( // Render IconButton only on small screens
-              <IconButton
-                icon={<ArrowBackIcon />}
-                onClick={() => setSelectedChat("")}
-              />
-            )}
-            <Box >
-              {!selectedChat.isGroupChat ? (
-                <>{getSender(user, selectedChat.users)}</>
-              ) : (
-                <>
-                  {selectedChat.chatName.toUpperCase()}
-                  <UpdateGroupChatModal
-                    fetchAgain={fetchAgain}
-                    setFetchAgain={setFetchAgain}
-                    fetchMessages={fetchMessages}
-                  />
-                </>
-              )}
-            </Box>
-
-            <Box>
-              {!selectedChat.isGroupChat && (
-                <ProfileModal
-                  user={getSenderDetails(user, selectedChat.users)}
+          {isSmallScreen && ( // Render IconButton only on small screens
+            <IconButton
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+          )}
+          <Box>
+            {!selectedChat.isGroupChat ? (
+              <>{getSender(user, selectedChat.users)}</>
+            ) : (
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                <UpdateGroupChatModal
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                  fetchMessages={fetchMessages}
                 />
-              )}
-            </Box>
+              </>
+            )}
+          </Box>
+
+          <Box>
+            {!selectedChat.isGroupChat && (
+              <ProfileModal user={getSenderDetails(user, selectedChat.users)} />
+            )}
+          </Box>
           {/* </Flex> */}
           <Box
             d="flex"
@@ -173,21 +208,17 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
               />
             ) : (
               <div className="messages">
-                <ScrollableChat messages={messages}/>
+                <ScrollableChat messages={messages} />
               </div>
             )}
-            <FormControl
-              onKeyDown={sendMessage}
-              isRequired
-              mt={3}
-            >
+            <FormControl onKeyDown={sendMessage} isRequired mt={3}>
               <Input
-              variant="filled"
-              bg="#E0E0E0"
-              placeholder="Enter a message...."
-              onChange={typingHandler}
-              value={newMessage}
-            />
+                variant="filled"
+                bg="#E0E0E0"
+                placeholder="Enter a message...."
+                onChange={typingHandler}
+                value={newMessage}
+              />
             </FormControl>
           </Box>
         </>
