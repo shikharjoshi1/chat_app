4a3239da0141ddf7440d7962a21f1567e1144df0
Shikhar Joshi <shikharjoshi10@gmail.com>
2024-04-03 21:06:34 -0400

    Created function for typing.. on server.js

--- backend/server.js
+++ backend/server.js
@@ -56,6 +56,10 @@ io.on("connection",(socket)=>{
     console.log("User Joined Room:"+ room);
   })
 
+  socket.on('typing',(room)=>socket.in(room).emit('typing'));
+  socket.on('stop typing',(room)=>socket.in(room).emit('stop typing'));
+
+
   socket.on('new message',(newMessageReceived)=>{
     var chat = newMessageReceived.chat;
 
